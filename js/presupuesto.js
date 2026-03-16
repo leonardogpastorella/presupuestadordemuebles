d58196d115a1e2fe5a35546c827ca0e32fa1bd40
leopast <leonardogpastorella@gmail.com>
2026-03-16 09:37:15 -0300

    Corrigiendo guardado de presupuestos

--- js/presupuesto.js
+++ js/presupuesto.js
@@ -25,29 +25,42 @@
         formaPago,
         total
     }
+    let presupuestos = JSON.parse(localStorage.getItem("presupuestos")) || [];
     presupuestos.push(presupuesto);
 localStorage.setItem("presupuestos", JSON.stringify(presupuestos));
 }
 
+
 function mostrarPresupuesto(){
+
     const contenedor = document.getElementById("contenidoPresupuesto")
-    if (presupuestos.length === 0)    {
-        contenedor.innerHTML = "<p>No hay presupuestos guardados</p>";
-        return;
+
+    const presupuestos = JSON.parse(localStorage.getItem("presupuestos")) || []
+
+    if (presupuestos.length === 0){
+        contenedor.innerHTML = "<p>No hay presupuestos guardados</p>"
+        return
     }
 
     contenedor.innerHTML = ""
-    presuspuestos.forEach(p => {
-        contenedor.innerHTML += `
-        div class="presupuesto">
-            <p>Cliente: ${p.nombreCliente}</p>
-            <p>Mueble: ${p.mueble}</p>
-            <p>total: $${p.total}</p>
 
-            <button onclick = "eliminarPresupuesto(${p.id})">Eliminar</button>
-        </div>  `
-            })
-        }
+    presupuestos.forEach((presupuesto, index) => {
+
+        const div = document.createElement("div")
+
+        div.innerHTML = `
+            <p><strong>Cliente:</strong> ${presupuesto.nombreCliente}</p>
+            <p>Mueble: ${presupuesto.mueble}</p>
+            <p>Total: $${presupuesto.total}</p>
+
+            <button onclick="eliminarPresupuesto(${index})">Eliminar</button>
+        `
+
+        contenedor.appendChild(div)
+
+    })
+
+}
 
     
 
@@ -55,12 +68,17 @@ function mostrarPresupuesto(){
     //document.getElementById("eliminarbtn").addEventListener("click", eliminarPresupuesto)
     //document.getElementById("enviarbtn").addEventListener("click", enviarPresupuesto)
 
-function eliminarPresupuesto(id) {
-    presupuestos = presupuestos.filter(p => p.id !== id);
-    localStorage.setItem("presupuestos", JSON.stringify(presupuestos));
-    mostrarPresupuesto();
-}
+function eliminarPresupuesto(index){
 
+    let presupuestos = JSON.parse(localStorage.getItem("presupuestos")) || []
+
+    presupuestos.splice(index,1)
+
+    localStorage.setItem("presupuestos", JSON.stringify(presupuestos))
+
+    mostrarPresupuesto()
+
+}
 function enviarPresupuesto() {
     alert("presupuesto enviado correctamente.")
     localStorage.removeItem("presupuesto");
